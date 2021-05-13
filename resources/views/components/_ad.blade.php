529469510d619993aed7db33de4681e4ad6f7398
FRANCESCO FERRILLO <francesco.ferrillo96@outlook.it>
2021-05-13 14:40:34 +0200

    Utente può essere un revisore

--- resources/views/components/_ad.blade.php
+++ resources/views/components/_ad.blade.php
@@ -1,22 +1,3 @@
-{{-- <div class="row justify-content-center mb-5">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header"><a href="{{$link}}" class="btn btn-primary">{{$title}}</a></div>
-        </div>
-        <div class="card-body">
-            <p>
-                <img src="https://via.placeholder.com/300x150.png" class="rounded float-right" alt="">
-                {{$body}}
-                {{$price}}
-            </p>
-        </div>
-        <div class="card-footer d-flex justify-content-between">
-            <strong>Category: <a
-                 href="{{$href}}">{{$category}}</a></strong>
-            <i>{{$date}} - {{$user}}</i>
-        </div>
-    </div>
-</div> --}}
 <div class="container">
     <div class="row">
         <div class="col-md-4">
