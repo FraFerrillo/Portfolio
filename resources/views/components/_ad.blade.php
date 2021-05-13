52b2099c967e4edd31b3ac3068122a9bd9b10348
FRANCESCO FERRILLO <francesco.ferrillo96@outlook.it>
2021-05-13 10:00:46 +0200

    card rifatte

--- resources/views/components/_ad.blade.php
+++ resources/views/components/_ad.blade.php
@@ -1,4 +1,4 @@
-<div class="row justify-content-center mb-5">
+{{-- <div class="row justify-content-center mb-5">
     <div class="col-md-8">
         <div class="card">
             <div class="card-header"><a href="{{$link}}" class="btn btn-primary">{{$title}}</a></div>
@@ -16,4 +16,20 @@
             <i>{{$date}} - {{$user}}</i>
         </div>
     </div>
+</div> --}}
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card my-3" style="width: 18rem;">
+                <img src="https://via.placeholder.com/300x150.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title"><a href="{{$link}}" class="btn btn-primary">{{$title}}</a></h5>
+                  <p class="card-text">{{$body}}</p>
+                  <h5>{{$price}}</h5>
+                  <strong>Category: <a href="{{$href}}">{{$category}}</a></strong><i>{{$date}} - {{$user}}</i>
+                </div>
+              </div>
+        </div>
+    </div>
 </div>
+
