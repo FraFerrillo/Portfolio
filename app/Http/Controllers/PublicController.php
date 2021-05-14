e3dc198a3012fd1f48e761b517536f42360fabf7
FRANCESCO FERRILLO <francesco.ferrillo96@outlook.it>
2021-05-14 17:01:12 +0200

    Queue fatta

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -15,13 +15,7 @@ class PublicController extends Controller
         ->take(6)->get();
         return view('welcome', compact('ads'));
     }
-    // public function search(Request $request)
-    // {
-    //     $q = $request->input('q');
 
-    //     $ads = Ad::search($q)->where('is_accepted', true)->get();
-    //     return view('ads.search_results', compact('q','ads'));
-    // }
     public function adByCategory($name, $category_id)
     {
         $category = Category::find($category_id);
